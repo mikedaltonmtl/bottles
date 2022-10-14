60e18739c8d9768391fee134b4e59382d5b0d5a6
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-10-14 12:59:10 +0000

    bottles.js completing task 1 and task 2

--- bottles.js
+++ bottles.js
@@ -5,32 +5,62 @@
  * For every four bottle caps, you can get one free (full) bottle of pop.
  * Each bottle of pop costs $2 to purchase.
  */
-const redemption = function(investment = process.argv[2]) {
+const bottles = function() {
 
-  let bottles = 0;
-  let caps = 0;
-  let result = 0;
+    // initialize return object
+    const result = {
+      drunkBottles: 0,
+      emptyBottles: 0,
+      caps: 0,
+    }
 
-  // base case
-  if (investment < 2) {
-    console.log('result', result);
-    return;
+  // validate investment and add to result
+  if (process.argv.length > 2) {
+
+    result['drunkBottles'] = Math.floor(process.argv[2] / 2);
+    result['emptyBottles'] = result['drunkBottles'];
+    result['caps'] = result['emptyBottles'];
+
+  } else {
+
+    return result;
   }
 
-  // recursive case
-  result = 
+  redeem(result);
 
-
-
-  
+  console.log("result['drunkBottles']", result['drunkBottles']);
 };
 
-redemption();
+const redeem = function(object) {
+  // base case
+  if (object['emptyBottles'] < 2 && object['caps'] < 4) return object;
+  
+  // recursive case
+  console.log('redeem', object);
+
+  // swap 2 empty bottles for one full bottle
+  if (object['emptyBottles'] >= 2) {
+
+    object['drunkBottles'] += 1;
+    object['emptyBottles'] -= 1; // 2 out, 1 in
+    object['caps'] += 1;
+  }
+
+  if (object['caps'] >= 4) {
+
+    object['drunkBottles'] += 1;
+    object['emptyBottles'] += 1;
+    object['caps'] -= 3; // 4 out, 1 in
+  }
+
+  // recall function for next iteration
+  redeem(object);
+};
+
+bottles();
 
 // expected output:
 // investment 10: bottles 15
 // investment 20: bottles 35
 // investment 30: bottles 55
 // investment 40: bottles 75
-
-module.exports = redemption;
